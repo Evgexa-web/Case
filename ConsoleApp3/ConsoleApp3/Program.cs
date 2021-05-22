02924d997bad9ed50f839da84e83114241e4b348
Evgexa-web <84617552+Evgexa-web@users.noreply.github.com>
2021-05-22 14:54:05 +0700

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k;
+            while (true)
+            {
+                Console.Write("k = ");
+                k = Console.ReadLine();
+                switch (k)
+                {
+                    case "1":
+                        Console.WriteLine("плохо");
+                        break;
+                    case "2":
+                        Console.WriteLine("неудовлетворительно");
+                        break;
+                    case "3":
+                        Console.WriteLine("удовлетворительно");
+                        break;
+                    case "4":
+                        Console.WriteLine("хорошо");
+                        break;
+                    case "5":
+                        Console.WriteLine("отлично");
+                        break;
+                    default:
+                        Console.WriteLine("ошибка");
+                        break;
+                }
+            }
+        }
+    }
+}
